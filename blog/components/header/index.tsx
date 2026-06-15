943e5e163c300379509df25131060379dd104bd0
Igor Araújo <igoritoaraujo223@gmail.com>
2026-06-15 13:59:07 -0300

    save

---
+++ blog/components/header/index.tsx
@@ -0,0 +1,10 @@
+'use client'
+export function Header() {
+    console.log('HEADER');
+    return (
+        <h1 className="text-6xl font-bold text-blue-600 hover:text-blue-50 hover:bg-blue-500 transition duration-1000">TEXTO OFICIAL DA HOMEPAGE</h1>
+    );
+
+}
+
+
